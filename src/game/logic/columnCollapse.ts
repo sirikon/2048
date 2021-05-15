d5814bb51ad7567caa6541a9e35793a47fd23281
Carlos Fdez. Llamas <hello@sirikon.me>
2021-05-15 15:55:50 +0200

    column collapse logic

--- src/game/logic/columnCollapse.ts
+++ src/game/logic/columnCollapse.ts
@@ -1,5 +1,32 @@
 import Cell from '../models/Cell'
 
 export function columnCollapse(column: Cell[][]): Cell[][] {
-	return column;
+	const result: Cell[][] = [];
+
+	for(let i = 0; i < column.length; i++) {
+		const cells = column[i];
+		if (cells.length === 0) continue;
+		const topCell = cells[0];
+
+		const nextTopCell = getNextTopCell(column, i);
+
+		if (nextTopCell != null && nextTopCell.cell.value === topCell.value) {
+			result.push([{ ...topCell, value: topCell.value*2 }])
+			i += nextTopCell.distance;
+		} else {
+			result.push([topCell]);
+		}
+	}
+
+	return result.concat(Array(column.length - result.length).fill(() => []).map(f => f()));
+}
+
+function getNextTopCell(column: Cell[][], position: number): { cell: Cell, distance: number } | null {
+	let i = 1;
+	while((i+position) < column.length) {
+		const cells = column[i+position];
+		if (cells.length > 0) return { cell: cells[0], distance: i }
+		i++;
+	}
+	return null;
 }
--- test/game/logic/columnCollapse.spec.ts
+++ test/game/logic/columnCollapse.spec.ts
@@ -1,11 +1,123 @@
 import { expect } from 'chai'
 
 import { columnCollapse } from '../../../src/game/logic/columnCollapse'
+import Cell from '../../../src/game/models/Cell';
 
-describe('Collumn Collapse', () => {
+describe('Column Collapse', () => {
 	
-	it('should work', () => {
-		expect(columnCollapse([])).to.deep.equal([]);
+	it('should work with empty columns', () => {
+		assertColumnCollapse([], [])
+		assertColumnCollapse([
+			[], [], [], []
+		], [
+			[], [], [], []
+		])
+	});
+
+	it('should work with cells', () => {
+		assertColumnCollapse([
+			[{ value: 2 }], [], [], []
+		], [
+			[{ value: 2 }], [], [], []
+		])
+	});
+
+	it('should work with moving single cells', () => {
+		assertColumnCollapse([
+			[{ value: 2 }], [], [], []
+		], [
+			[{ value: 2 }], [], [], []
+		])
+
+		assertColumnCollapse([
+			[], [{ value: 2 }], [], []
+		], [
+			[{ value: 2 }], [], [], []
+		])
+
+		assertColumnCollapse([
+			[], [], [], [{ value: 2 }]
+		], [
+			[{ value: 2 }], [], [], []
+		])
+	});
+
+	it('should work with moving multiple cells', () => {
+		assertColumnCollapse([
+			[{ value: 2 }], [{ value: 4 }], [], []
+		], [
+			[{ value: 2 }], [{ value: 4 }], [], []
+		])
+
+		assertColumnCollapse([
+			[{ value: 2 }], [], [], [{ value: 4 }]
+		], [
+			[{ value: 2 }], [{ value: 4 }], [], []
+		])
+
+		assertColumnCollapse([
+			[], [{ value: 2 }], [], [{ value: 4 }]
+		], [
+			[{ value: 2 }], [{ value: 4 }], [], []
+		])
+
+		assertColumnCollapse([
+			[], [], [{ value: 2 }], [{ value: 4 }]
+		], [
+			[{ value: 2 }], [{ value: 4 }], [], []
+		])
+	});
+
+	it('shold work with combining cells', () => {
+		assertColumnCollapse([
+			[{ value: 2 }], [{ value: 2 }], [], []
+		], [
+			[{ value: 4 }], [], [], []
+		])
+
+		assertColumnCollapse([
+			[], [], [{ value: 2 }], [{ value: 2 }]
+		], [
+			[{ value: 4 }], [], [], []
+		])
+
+		assertColumnCollapse([
+			[{ value: 2 }], [], [], [{ value: 2 }]
+		], [
+			[{ value: 4 }], [], [], []
+		])
+
+		assertColumnCollapse([
+			[{ value: 2 }], [], [{ value: 4 }], [{ value: 2 }]
+		], [
+			[{ value: 2 }], [{ value: 4 }], [{ value: 2 }], []
+		])
+	});
+
+	it('shold work with combining multiple cells', () => {
+		assertColumnCollapse([
+			[{ value: 2 }], [{ value: 2 }], [{ value: 2 }], [{ value: 2 }]
+		], [
+			[{ value: 4 }], [{ value: 4 }], [], []
+		])
+	});
+
+	it('shold work with combining and moving cells', () => {
+		assertColumnCollapse([
+			[], [{ value: 4 }], [{ value: 2 }], [{ value: 2 }]
+		], [
+			[{ value: 4 }], [{ value: 4 }], [], []
+		])
+
+		assertColumnCollapse([
+			[], [{ value: 2 }], [{ value: 2 }], [{ value: 4 }]
+		], [
+			[{ value: 4 }], [{ value: 4 }], [], []
+		])
 	});
 
 })
+
+function assertColumnCollapse(input: Cell[][], output: Cell[][]) {
+	expect(columnCollapse(input)).to.deep.equal(output);
+}
